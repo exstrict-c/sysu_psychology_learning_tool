77aa9b76fdf3bccf8f42f0d8f6815a5f7206830f
exstrict-c <2549424044@qq.com>
2026-07-09 01:10:32 +0800

    second commit 2026/7/9

--- main.js
+++ main.js
@@ -14,18 +14,21 @@ function goToNextPage(button_next) {
         }
 }
 
+
 (function () {
     'use strict';
-    
+    console.log("tool launched");
+
     let button_play = document.querySelector('[title="播放"]');
     let button_next = document.querySelector('[id="next-activity-link"]');
     let video = document.querySelector("video");
-    
+
     if(button_play) {
-        button_play.click(button_next);
-        video.addEventListener("ended",goToNextPage)
+        console.log("video start");
+        button_play.click();
+        video.addEventListener("ended",()=>goToNextPage(button_next))
     }else {
         goToNextPage(button_next);
     }
-    
+
 })();
